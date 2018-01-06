2d362a3b9d0a3eb98045de2188b5deab203e3249
Daniels Pitkevičs <daniels.pitkevics@gmail.com>
2018-01-06 12:55:10 +0100

    Added prefix validation when creating car

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php
 
+// We define a ID prefix that should be used when creating a car
+const ID_PREFIX = 'CAR';
+
 /**
- * @param int|null $id
+ * @param string|null $id
  *
  * @return array
  */
-function createCar(int $id = null): array
+function createCar(string $id = null): ?array
 {
     // We check `$id` variable is passed or not. If it is not passed, we set it to a generated unique ID
     if (!$id) {
-        $id = uniqid(); // `uniqid` is a function that generates a unique ID and returns
+        $id = ID_PREFIX . uniqid(); // `uniqid` is a function that generates a unique ID and returns
+    } else {
+        if (strpos($id, ID_PREFIX) !== 0) {
+            return null;
+        }
     }
 
     $car = [
@@ -32,7 +39,13 @@ function driveCar(array &$car): void
     $car['position']['y'] += 2;
 }
 
-$car = createCar(); // we create a car
+$car = createCar('CAR125'); // we create a car
+// We check if car is actually created or not and echo message and exit if it is not created
+if (!$car) {
+    echo 'Car is not built';
+    exit;
+}
+
 driveCar($car); // we drive that car
 
 var_dump($car);
